a1ee73b737b584c42c3d62964c1b1a86d03144f5
Ted Lawson <big.tedde@gmail.com>
2022-10-09 16:59:03 -0700

    Initial Commit

---
+++ Gambling Simulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gambling Simulator
+//
+//  Created by Theodore Lawson on 10/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
